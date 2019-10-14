273338052f6f3f00b277ca391af790427528cb8d
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-14 18:14:13 -0400

    movie cards linked to individual movies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,23 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const MovieList = props => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
+        .get("http://localhost:5000/api/movies")
         .then(response => {
           setMovies(response.data);
         })
         .catch(error => {
-          console.error('Server Error', error);
+          console.error("Server Error", error);
         });
-    }
-    
+    };
+
     getMovies();
   }, []);
-  
+
   return (
     <div className="movie-list">
       {movies.map(movie => (
@@ -25,12 +26,12 @@ const MovieList = props => {
       ))}
     </div>
   );
-}
+};
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <Link to={`/movies/${movie.id}`} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -45,7 +46,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div>
+    </Link>
   );
 }
 
