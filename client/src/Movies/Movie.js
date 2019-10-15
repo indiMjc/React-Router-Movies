23eec3c3f1071c917e8c440a7cbc11603c8bff79
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-15 00:35:43 -0400

    movie card component written for stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import MovieCard from "./MovieCard";
 
 const Movie = props => {
   const [movie, setMovie] = useState(null);
-  console.log(props.match.params.id);
 
   useEffect(() => {
     const id = props.match.params.id;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
 
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
@@ -18,7 +16,7 @@ const Movie = props => {
       .catch(error => {
         console.error(error);
       });
-  }, []);
+  }, [props.match.params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -33,7 +31,16 @@ const Movie = props => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
+      <MovieCard info={movie} />
+      <div className="save-button">Save</div>
+    </div>
+  );
+};
+
+export default Movie;
+
+{
+  /* <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -48,10 +55,5 @@ const Movie = props => {
             {star}
           </div>
         ))}
-      </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
-};
-
-export default Movie;
+      </div> */
+}
